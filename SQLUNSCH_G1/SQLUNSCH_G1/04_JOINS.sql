90105371144150a5547fcbd12ea51ba201b6212b
Stivens Espinoza Reina <stivens70espinoza@gmail.com>
2026-05-23 16:58:51 -0500

    Inner Join P1

--- SQLUNSCH_G1/SQLUNSCH_G1/04_JOINS.sql
+++ SQLUNSCH_G1/SQLUNSCH_G1/04_JOINS.sql
@@ -1,5 +1,8 @@
 USE AdventureWorks2022
 GO
+--=================================================================
+--						1RA PARTE
+--=================================================================
 SELECT TOP 100 
 	PRO.Name AS Producto,
     PROSU.Name AS Subcategoria,
@@ -25,3 +28,96 @@ INNER JOIN Production.ProductSubcategory AS PROSU
     ON PRO.ProductSubcategoryID = PROSU.ProductSubcategoryID
 INNER JOIN [Production].[ProductCategory] AS PROCAT
 	ON PROSU.ProductCategoryID=PROCAT.ProductCategoryID
+
+--=================================================================
+--						2DA PARTE
+--=================================================================
+SELECT
+	DISTINCT ProductSubcategoryID
+FROM [Production].[Product]
+
+SELECT 
+    PRO.Name as Producto
+    , PRO.ProductNumber
+	, SUB.Name as Subcategoria
+	, CAT.Name as Categoria
+FROM [Production].[Product] AS PRO
+INNER JOIN [Production].[ProductSubcategory] AS SUB
+	ON PRO.ProductSubcategoryID=SUB.ProductSubcategoryID
+INNER JOIN [Production].[ProductCategory] AS CAT
+	ON CAT.ProductCategoryID=SUB.ProductCategoryID
+--WHERE ProductSubcategoryID IS NOT NULL
+
+
+--select*from [Production].[ProductSubcategory]
+--SELECT*FROM [Production].[ProductCategory]
+
+
+-- [Production].[Product] - [Production].[ProductListPriceHistory]
+--SELECT
+--	PRO.ProductID AS ProductKey,
+--	PRO.Name AS Producto,
+--	PRO.ListPrice AS PrecioFinal,
+--	HIST.ListPrice AS PrecioHistorico,
+--	HIST.StartDate,HIST.EndDate
+--FROM [Production].[Product] AS PRO
+--INNER JOIN [Production].[ProductListPriceHistory] AS HIST
+--	ON PRO.ProductID=HIST.ProductID
+
+--=================================================================
+--						LEFT/RIGHT
+--=================================================================
+
+
+--SELECT 
+--    PRO.Name as Producto
+--    , PRO.ProductNumber
+--	, SUB.Name as Subcategoria
+--FROM  [Production].[ProductSubcategory] AS SUB 
+--RIGHT JOIN [Production].[Product] AS PRO
+--	ON PRO.ProductSubcategoryID=SUB.ProductSubcategoryID
+
+--SELECT*FROM [Production].[ProductSubcategory]
+
+--=================================================================
+--						FULL OUTER JOIN
+--=================================================================
+
+
+SELECT 
+    PRO.Name as Producto
+    , PRO.ProductNumber
+	, SUB.Name as Subcategoria
+FROM  [Production].[ProductSubcategory] AS SUB 
+CROSS JOIN [Production].[Product] AS PRO
+	--ON PRO.ProductSubcategoryID=SUB.ProductSubcategoryID
+
+-- FULL OUTER JOIN
+-- jugando con cross join
+SELECT 
+     [ProductID]
+    , [Name]    
+    , [ProductSubcategoryID]
+	INTO #TEMPORALPRODUCTO
+FROM [Production].[Product] 
+WHERE [ProductSubcategoryID] IN (17,14,12)
+
+
+SELECT 
+     [ProductSubcategoryID]
+    , [Name]
+	INTO #TEMPORALSUBCATEGORIA
+FROM [Production].[ProductSubcategory]
+WHERE [ProductSubcategoryID] IN (17,14,12)
+
+
+
+SELECT*FROM #TEMPORALPRODUCTO
+SELECT*FROM #TEMPORALSUBCATEGORIA
+SELECT 
+	PRO.ProductID,
+	PRO.ProductSubcategoryID,
+	SUB.ProductSubcategoryID,
+	PRO.Name
+FROM  #TEMPORALPRODUCTO AS PRO 
+CROSS JOIN #TEMPORALSUBCATEGORIA AS SUB
